dc059170ed4d42c80f1359d66935c65d531c8cd6
KopylP <p.kopyl9@gmail.com>
2023-11-11 16:59:19 +0100

    Code refactor

---
+++ Horoscope.Admin.Bot/ServiceCollectionExtensions.cs
@@ -0,0 +1,62 @@
+using Google.Cloud.Firestore;
+using Horoscope.Admin.Bot.Framework.Chains;
+using Horoscope.Admin.Bot.Framework.Persistence;
+using Horoscope.Admin.Bot.Handlers;
+using Horoscope.Admin.Bot.Models;
+using Horoscope.Admin.Bot.Services;
+using Telegram.Bot;
+
+namespace Horoscope.Admin.Bot;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddChainHandlers(this IServiceCollection services)
+    {
+        services
+            .Chain<IChainOfResponsibilityHandler<NewtonsoftJsonUpdate>>()
+            .Add<ExecutionContextSetupHandler>()
+            .Add<StartHandler>()
+            .Add<ApiKeyVerificationHandler>()
+            .Add<StartingEditHandler>()
+            .Add<BackHandler>()
+            .Add<WaitingForDateHandler>()
+            .Add<CancelHandler>()
+            .Add<WaitingForSignHandler>()
+            .Add<PreviewHandler>()
+            .Add<WaitingForForecastHandler>()
+            .Add<ForecastProvidedHandler>()
+            .Configure();
+
+        return services;
+    }
+
+    public static IServiceCollection AddFirestoreDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var firebaseSettings = configuration.GetRequiredSection("FirebaseSettings").Get<FirebaseSettings>()!;
+        
+        services.AddSingleton<FirestoreDb>(_ => new FirestoreDbBuilder
+        {
+            ProjectId = firebaseSettings.ProjectId,
+            CredentialsPath = firebaseSettings.JsonCredentials
+        }.Build());
+
+        services.AddScoped<FirestoreProvider>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddTelegram(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var botSettings = configuration.GetRequiredSection("BotConfiguration").Get<BotSettings>()!;
+        services.AddSingleton(botSettings);
+        services.AddHttpClient("TelegramWebhook")
+            .AddTypedClient<ITelegramBotClient>(httpClient => new TelegramBotClient(botSettings.BotToken!, httpClient));
+        services.AddHostedService<ConfigureWebhook>();
+        
+        return services;
+    }
+}
---
+++ Horoscope.Admin.Bot/WebApplicationExtensions.cs
@@ -0,0 +1,27 @@
+using Horoscope.Admin.Bot.Framework.Chains;
+using Horoscope.Admin.Bot.Framework.Extensions;
+using Horoscope.Admin.Bot.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Horoscope.Admin.Bot;
+
+public static class WebApplicationExtensions
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        var botSettings = app.Services.GetRequiredService<BotSettings>();
+
+        app.MapPost($"/bot/{botSettings.EscapedBotToken}", async (
+                NewtonsoftJsonUpdate update,
+                [FromServices] IChainOfResponsibilityHandler<NewtonsoftJsonUpdate> handler) =>
+            {
+                if (string.IsNullOrWhiteSpace(update.GetMessage()))
+                    return Results.Ok();
+
+                await handler.HandleAsync(update);
+
+                return Results.Ok();
+            })
+            .WithName("TelegramWebhook");
+    }
+}
